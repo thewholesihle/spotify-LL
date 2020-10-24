23daef55fa2ecf9cba532c7e2c61b75346d41f1c
trinity-senpai <45470217+trinitysenpai@users.noreply.github.com>
2020-10-23 15:37:09 -0900

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,31 @@
+/*
+
+Hope You liked it!
+I used a js library to create a sweet popup called SweetAlert !
+
+*/
+//The sign up alert
+function signupAlert() {
+
+Swal.fire
+({
+    title:'Sign Up Complete!',
+    icon:'success',
+    confirmButtonText:'Continue'
+})
+
+// function close
+}
+//The Login alert
+function loginAlert() {
+
+Swal.fire
+({
+    title:'Login Success!',
+    icon:'success',
+    confirmButtonText:'Continue'
+})
+
+//function close
+}
+
